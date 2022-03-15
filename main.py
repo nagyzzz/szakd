1aa976cf6c5f1d8db19d8963d38b4493d47a673a
nagyzzz <nagyzzz1977@gmail.com>
2022-03-15 20:20:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QTextEdit, QAction, QTabBar, QPushButton
+from PyQt5 import uic
+import sys
+
+class main_window(QMainWindow):
+    def __init__(self):
+        super(main_window, self).__init__()
+        uic.loadUi("main_window.ui", self)
+        self.show()
+
+class file_chooser(QMainWindow):
+    def __init__(self):
+        super(file_chooser, self).__init__()
+        uic.loadUi("file_chooser.ui", self)
+        self.show()
+
+class file_viewer(QMainWindow):
+    def __init__(self):
+        super(file_viewer, self).__init__()
+        uic.loadUi("file_chooser.ui", self)
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    UiWindow = main_window()
+    #UiWindow = file_chooser()
+    UiWindow.show()
+    sys.exit(app.exec())
+
+
+
+
+
+
